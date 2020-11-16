5cb1e2b95700ebdf35b7ce55af92afb7bea7f74b
Opilane TTHK <opilane@tthk.ee>
2020-11-16 15:04:11 +0200

    first commit

---
+++ Filesproperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Filesproperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+                var fileData = new FileInfo(file);
+                Console.WriteLine(fileData);
+                string fileName = fileData.Name;
+                string fileDirectory = fileData.DirectoryName;
+                long fileSize = fileData.Length;
+
+                Console.WriteLine($"File name :{fileName}; location:{fileDirectory}; Size: {fileSize}");
+
+            }
+
+
+
+        }
+    }
+}
